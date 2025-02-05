2afc37be20455b16d819b243d512d4a35e89c2ca
Rafael Pacheco <rafaelpache64@outlook.com>
2025-02-05 17:08:13 -0400

    Reduced the amount of code by adding a for loop.
    In the buttons_config.py there is the array with all the buttons
    information.

--- calculator.py
+++ calculator.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 import re
+from buttons_config import buttons_array
 
 
 class Calculator:
 
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("500x500")
+        self.root.geometry("700x700")
         self.root.title("Calculator by rafa64H")
 
         self.result = tk.Label(self.root, text="0", font={"arial", 18})
@@ -29,149 +30,26 @@ class Calculator:
         self.buttonframe.columnconfigure(2, weight=1)
         self.buttonframe.columnconfigure(3, weight=1)
 
-        self.btn0 = tk.Button(
-            self.buttonframe,
-            text="0",
-            font={"arial", 16},
-            command=lambda: self.button_click("0"),
-        )
-        self.btn0.grid(row=4, column=0, columnspan=2, sticky=tk.W + tk.E)
-
-        self.btn1 = tk.Button(
-            self.buttonframe,
-            text="1",
-            font={"arial", 16},
-            command=lambda: self.button_click("1"),
-        )
-        self.btn1.grid(row=3, column=0, sticky=tk.W + tk.E)
-
-        self.btn2 = tk.Button(
-            self.buttonframe,
-            text="2",
-            font={"arial", 16},
-            command=lambda: self.button_click("2"),
-        )
-        self.btn2.grid(row=3, column=1, sticky=tk.W + tk.E)
-
-        self.btn3 = tk.Button(
-            self.buttonframe,
-            text="3",
-            font={"arial", 16},
-            command=lambda: self.button_click("3"),
-        )
-        self.btn3.grid(row=3, column=2, sticky=tk.W + tk.E)
-
-        self.btn4 = tk.Button(
-            self.buttonframe,
-            text="4",
-            font={"arial", 16},
-            command=lambda: self.button_click("4"),
-        )
-        self.btn4.grid(row=2, column=0, sticky=tk.W + tk.E)
-
-        self.btn5 = tk.Button(
-            self.buttonframe,
-            text="5",
-            font={"arial", 16},
-            command=lambda: self.button_click("5"),
-        )
-        self.btn5.grid(row=2, column=1, sticky=tk.W + tk.E)
-
-        self.btn6 = tk.Button(
-            self.buttonframe,
-            text="6",
-            font={"arial", 16},
-            command=lambda: self.button_click("6"),
-        )
-        self.btn6.grid(row=2, column=2, sticky=tk.W + tk.E)
-
-        self.btn7 = tk.Button(
-            self.buttonframe,
-            text="7",
-            font={"arial", 16},
-            command=lambda: self.button_click("7"),
-        )
-        self.btn7.grid(row=1, column=0, sticky=tk.W + tk.E)
-
-        self.btn8 = tk.Button(
-            self.buttonframe,
-            text="8",
-            font={"arial", 16},
-            command=lambda: self.button_click("8"),
-        )
-        self.btn8.grid(row=1, column=1, sticky=tk.W + tk.E)
-
-        self.btn9 = tk.Button(
-            self.buttonframe,
-            text="9",
-            font={"arial", 16},
-            command=lambda: self.button_click("9"),
-        )
-        self.btn9.grid(row=1, column=2, sticky=tk.W + tk.E)
-
-        self.btndot = tk.Button(
-            self.buttonframe,
-            text=".",
-            font={"arial", 16},
-            command=lambda: self.button_click("."),
-        )
-        self.btndot.grid(row=4, column=2, sticky=tk.W + tk.E)
-
-        self.btnSubstract = tk.Button(
-            self.buttonframe,
-            text="-",
-            font={"arial", 16},
-            command=lambda: self.button_click("-"),
-        )
-        self.btnSubstract.grid(row=1, column=3, sticky=tk.W + tk.E)
-
-        self.btnPlus = tk.Button(
-            self.buttonframe,
-            text="+",
-            font={"arial", 16},
-            command=lambda: self.button_click("+"),
-        )
-        self.btnPlus.grid(row=0, column=3, sticky=tk.W + tk.E)
-
-        self.btnDivide = tk.Button(
-            self.buttonframe,
-            text="/",
-            font={"arial", 16},
-            command=lambda: self.button_click("/"),
-        )
-        self.btnDivide.grid(row=0, column=2, sticky=tk.W + tk.E)
-
-        self.btnMultiply = tk.Button(
-            self.buttonframe,
-            text="*",
-            font={"arial", 16},
-            command=lambda: self.button_click("*"),
-        )
-        self.btnMultiply.grid(row=0, column=1, sticky=tk.W + tk.E)
-
-        self.equal = tk.Button(
-            self.buttonframe,
-            text="Backspace",
-            font={"arial", 16},
-            command=lambda: self.button_click("backspace"),
-        )
-        self.equal.grid(row=2, column=3, sticky=tk.W + tk.E + tk.N + tk.S)
-
-        self.equal = tk.Button(
-            self.buttonframe,
-            text="C",
-            font={"arial", 16},
-            command=lambda: self.button_click("c"),
-        )
-        self.equal.grid(row=0, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
-
-        self.equal = tk.Button(
-            self.buttonframe,
-            text="=",
-            font={"arial", 16},
-            command=lambda: self.button_click("="),
-        )
-        self.equal.grid(row=3, column=3, rowspan=2, sticky=tk.W + tk.E + tk.N + tk.S)
+        for button_item in buttons_array:
+            new_btn = tk.Button(
+                self.buttonframe,
+                text=button_item["text"],
+                font={"arial", 16},
+                command=lambda text=button_item["text"]: self.button_click(text),
+            )
+            grid_options = {
+                "row": button_item["row"],
+                "column": button_item["column"],
+            }
+            if button_item["sticky"] == "left_right":
+                grid_options["sticky"] = tk.W + tk.E
+            if button_item["sticky"] == "up_down_left_right":
+                grid_options["sticky"] = tk.N + tk.S + tk.W + tk.E
+            if button_item["columnspan"] != False:
+                grid_options["columnspan"] = button_item["columnspan"]
+            if button_item["rowspan"] != False:
+                grid_options["rowspan"] = button_item["rowspan"]
+            new_btn.grid(**grid_options)
 
         self.buttonframe.pack(fill="x")
 
@@ -189,8 +67,12 @@ class Calculator:
             return True
 
     def input_root_binded(self, event):
-        if event.keysym == 'Return' or event.keysym == 'KP_Enter' or event.keysym == 'equal':
-            self.button_click('=')
+        if (
+            event.keysym == "Return"
+            or event.keysym == "KP_Enter"
+            or event.keysym == "equal"
+        ):
+            self.button_click("=")
         if not self.root.focus_get() == self.entry:
 
             if event.keysym == "BackSpace":
@@ -206,14 +88,14 @@ class Calculator:
 
     def button_click(self, text):
 
-        if text != "backspace" and text != "=" and text != "c":
+        if text != "Backspace" and text != "=" and text != "c":
             current_text = self.entry.get()
             new_text = f"{current_text}{text}"
             self.entry.config(validate="none")
             self.entry.delete(0, tk.END)
             self.entry.insert(0, new_text)
             self.entry.config(validate="key")
-        elif text == "backspace":
+        elif text == "Backspace":
             current_text = self.entry.get()
             new_text = current_text[:-1]
             self.entry.config(validate="none")
@@ -230,8 +112,8 @@ class Calculator:
             raw_string = self.entry.get()
             pattern = r"\-|\+|\*|/"
 
-            pattern_invalid_zero = r'\b0\d+|[/*\-+]0\d+'
-            pattern_invalid_dots = r'(?<!\d)\.(?!\d)|\.\.|\.\d+\.|[/*\-+]\.[/*\-+]'
+            pattern_invalid_zero = r"\b0\d+|[/*\-+]0\d+"
+            pattern_invalid_dots = r"(?<!\d)\.(?!\d)|\.\.|\.\d+\.|[/*\-+]\.[/*\-+]"
 
             find_invalid_zero = bool(re.search(pattern_invalid_zero, raw_string))
             find_invalid_dots = bool(re.search(pattern_invalid_dots, raw_string))
@@ -239,10 +121,10 @@ class Calculator:
             if "/0" in raw_string:
                 self.result.config(text="Can not divide by 0")
                 return
-            if(find_invalid_zero):
+            if find_invalid_zero:
                 self.result.config(text="Invalid expression, invalid zero")
                 return None
-            if(find_invalid_dots):
+            if find_invalid_dots:
                 self.result.config(text="Invalid expression, wrong dot location")
                 return None
 
@@ -261,5 +143,6 @@ class Calculator:
             result = eval(raw_string)
 
             self.result.config(text=str(result))
+
 
 Calculator()
